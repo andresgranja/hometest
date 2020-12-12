ca1b3c1c5e1904fd6da09baf77372629c8e2a828
Andres Granja <andresgranja001@gmail.com>
2020-12-12 00:32:13 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CurrencylistComponent } from './components/currencylist/currencylist.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'currency', component: CurrencylistComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/currencylist/currencylist.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currencylist',
+  templateUrl: './currencylist.component.html'
+})
+export class CurrencylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
